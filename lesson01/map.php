5e9d2f45d52c8480fe773d0a87ef2de25c9fb2d8
Yumi Kimura <kimurayumixy@gmail.com>
2023-05-10 18:25:05 +0900

    commit

--- lesson01/map.php
+++ lesson01/map.php
@@ -44,48 +44,61 @@ $makers = $stmt_maker->fetchAll(PDO::FETCH_ASSOC);
     <div id="map"></div>
 
     <script>
-        const map = new mapboxgl.Map({
-            container: 'map',
-            // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: [139.6917, 35.6895], // 東京の座標
-            zoom: 6, // ズームレベル
-            bounds: [
-            [139.5546, 35.5707], // 西側の経度、南側の緯度
-            [139.9406, 35.8244]  // 東側の経度、北側の緯度
-            ]
-        });
+    const map = new mapboxgl.Map({
+        container: 'map',
+        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [139.6917, 35.6895], // 東京の座標
+        zoom: 6, // ズームレベル
+        bounds: [
+        [139.5546, 35.5707], // 西側の経度、南側の緯度
+        [139.9406, 35.8244]  // 東側の経度、北側の緯度
+        ]
+    });
 
-        // Add the control to the map.
-        map.addControl(
-            new MapboxGeocoder({
-                accessToken: mapboxgl.accessToken,
-                mapboxgl: mapboxgl
-            })
-        );
-        const geocoder = new MapboxGeocoder({
+    // Add the control to the map.
+    map.addControl(
+        new MapboxGeocoder({
             accessToken: mapboxgl.accessToken,
             mapboxgl: mapboxgl
-        });
+        })
+    );
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    });
 
-        // Add the control to the map.
-        map.addControl(geocoder);
-        // ジオコーダーで住所を検索し、マーカーを追加する
-        <?php foreach ($makers as $maker) : ?>
-            var address = '<?php echo $maker["address"]; ?>';
+    // Add the control to the map.
+    map.addControl(geocoder);
+    // ジオコーダーで住所を検索し、マーカーを追加する
+    const promises = [];
+    <?php foreach ($makers as $maker) : ?>
+        var address = '<?php echo $maker["address"]; ?>';
+        promises.push(new Promise(function(resolve, reject) {
             geocoder.query(address, function (err, data) {
                 if (err) {
-                console.error(err);
-                return;
+                    reject(err);
+                } else {
+                    resolve(data);
                 }
-
-                var marker = new mapboxgl.Marker({
-                color: 'orange'
-                })
-                .setLngLat(data.features[0].center)
-                .addTo(map);
             });
-            <?php endforeach; ?>
+        }));
+    <?php endforeach; ?>
+
+    Promise.all(promises)
+    .then(function(dataArray) {
+        for (let i = 0; i < dataArray.length; i++) {
+            const data = dataArray[i];
+            var marker = new mapboxgl.Marker({
+                color: 'orange'
+            })
+            .setLngLat(data.features[0].center)
+            .addTo(map);
+        }
+    })
+    .catch(function(error) {
+        console.error(error);
+    });
     </script>
 </body>
 </html>
